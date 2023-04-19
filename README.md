28182cf75e6da8c0aed108d4f9accd16c45c3da3
Derek He <dhe@voip.com.au>
2023-04-19 11:04:22 +1000

    improve code and doc

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# state_machine
+# State Machine Visualization
+This program connects to a server and simulates a state machine by sending and receiving data and finally create a pdf file called "state_machine.pdf".
+
+>This program utilizes a random number generator to produce actions randomly ranging from 1 to 3.
+
+## Requirements
+- Python 3.6 and above installed on your local machine
+- Graphviz installed on your local machine
+
+## Installation
+1. Install venv
+   ```bash
+   python3 -m venv venv
+   ```
+2. Activate venv
+   ```bash
+   source ./venv/bin/activate
+   ```
+3. Install dependencies
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+## How to run the program?
+After activating the venv, run the command
+```bash
+python state_machine.py
+```
+
+#### Output:
+- state_machine
+- state_machine.pdf
--- state_machine.py
+++ state_machine.py
@@ -2,9 +2,8 @@ import socket
 from graphviz import Digraph
 import random
 
-# connect to the server
+# create a TCP/IP socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 
 # create a new graph
 dot = Digraph()
@@ -16,11 +15,14 @@ data = 'A'
 dot.node(data, shape='circle')
 
 try:
+    # connect to the server
     s.connect(('20.28.230.252', 65432))
-    while True:
+    
+    # loop until we reach the terminal state
+    while data != 'Z':
         # generate a random action
         action = str(random.randint(1, 3))
-        print(f"Server：{data}")
+        print(f"Server: {data}")
         print(f"Client: {action}")
         # send the action to the server
         s.sendall(f"{action}\n".encode())
@@ -28,20 +30,22 @@ try:
         # get the new state from the server
         data = s.recv(1024).decode().strip()[0]
         # add the current state and transition to the graph
-        dot.node(data, shape='circle' if data == 'A' or data == 'Z' else 'circle')
+        dot.node(data, shape='circle')
         dot.edge(prev_data, data, label=action)
-        # check if we have reached the terminal state
-        if data == 'Z':
-            print(f"Server：{data}")
-            dot.node(data, shape='circle')
-            dot.edge(data, 'A', label='')
-            break
-
+    
+    # add the final state to the graph
+    dot.node(data, shape='circle')
+    dot.edge(data, 'A', label='')
+    
     # render the graph and save it as a PDF
     dot.render('state_machine', format='pdf')
+    print(f"Server: {data}")
+    print(f"Back to Server: A")
+    print(f"End!")
 except socket.error as e:
     print(f'Error connecting to server: {e}')
 except Exception as e:
     print(f'Error: {e}')
 finally:
+    # close the socket connection
     s.close()
